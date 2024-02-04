e9f16c3e5f1ab0ab68293ed7e54d37f39f55b2fa
earth15354 <jitpuwapat_mokkamakkul@brown.edu>
2024-02-03 23:25:11 -0500

    added frontend folder

---
+++ earth-flat-front/src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/App.css';
+
+function Main() {
+    return (
+        <div className="Main">
+            <p className="Main-Head">
+                <h1>Main</h1>
+            </p>
+        </div>
+    );
+}
+
+export default Main;
